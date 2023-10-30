e756e294c92e0a7ac27e3e5c5288f36e6a4ff7e7
Jose Von Chong <javch1995@gmail.com>
2023-10-30 12:59:26 -0500

    init for copa black friday

---
+++ src/routes/+page.svelte
@@ -0,0 +1,44 @@
+<div class="w-full bg-primary min-h-[256px] grid grid-rows-[64px,auto,64px]">
+	<div class="row-start-2">
+		<div class="container mx-auto">	
+				<nav aria-label="Breadcrumb" class="!mb-24">
+					<ol class="Breadcrumb flex gap-8 md:gap-16 items-center">
+					<li class="text-grey-75 font-sans hover:underline active:underline">
+						<a href="copa.com">Inicio</a>
+					</li>
+					<li class="text-grey-75 font-sans">›</li>
+					<li class="text-grey-75 font-sans hover:underline active:underline">
+						<a href="destinationsguide.copaair.com/en/deals">Promociones</a>
+					</li>
+					</ol>
+				</nav>
+                <h1 class="font-heading font-heading-bold text-grey-75 text-h1-sm lg:text-h1-lg md:text-h1-md">Copa Black Friday</h1>	
+           
+            </div>
+	</div>
+</div>
+<div class="w-full my-32">
+    <div class="container mx-auto">
+        	
+        <h2 class="!font-heading font-heading-bold text-primary text-h2-sm lg:text-h2-lg md:text-h2-md text-center ">¡<span class="text-grey-800">Black Friday</span>, las mejores tarifas del año! <br>Descubre nuestras ofertas imperdibles y ahorra como nunca.</h2>	
+    
+        <div class="my-16">
+            <h3 class="!font-heading font-heading-bold !text-secondary text-h3-sm lg:text-h3-lg md:text-h3-md text-center ">
+                Faltan:</h3>
+          
+            <h3 class="!font-heading font-heading-bold !text-secondary text-h3-sm lg:text-h3-lg md:text-h3-md text-center ">
+                Para <span class="text-grey-800">Black Friday</span></h3>
+        </div>
+        
+    
+    
+            <h3 class="font-heading font-heading-bold text-primary-dark text-h3-sm lg:text-h3-lg md:text-h3-md text-center  my-32">
+                Guarda esta pagina en tu favoritos para futuras tarifas flash.</h3>
+           
+
+            <div class="flex justify-center items-center ">
+                <button class="button" type="button">Guardar en favoritos</button>
+              </div>
+              
+    </div>
+</div>
