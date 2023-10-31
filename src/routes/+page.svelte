b2bfc7bef51034fd2de957e4fe86155ff17da0c2
Jose Von Chong <javch1995@gmail.com>
2023-10-31 08:41:52 -0500

    final changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,7 +13,7 @@
 					</ol>
 				</nav>
                 <h1 class="font-heading font-heading-bold text-grey-75 text-h1-sm lg:text-h1-lg md:text-h1-md mb-8">Un <span class="!text-secondary">Black Friday</span> lleno de Ofertas</h1>	
-                <h2 class=" font-sans text-caption md:text-caption-large text-grey-100">Estamos preparando una selección de tarifas en vuelos que te dejarán sin aliento. ¡Mantente atento <br> y no te pierdas de un Black Friday lleno de ofertas que te esperan!</h2>
+                <h2 class=" font-sans text-18/24 text-grey-100">Estamos preparando una selección de tarifas en vuelos que te dejarán sin aliento. ¡Mantente atento <br> y no te pierdas de un Black Friday lleno de ofertas que te esperan!</h2>
 
             </div>
 	</div>
@@ -33,14 +33,15 @@
         </h4> 
 
 
-        <h3 class="!font-heading font-heading-bold text-primary text-h3 md:text-h3-lg  pt-64  ">Completa tus datos para recibir nuestras promociones
-        </h3>
+       
 
-        <div class="flex justify-between items-start">
-            <img src="https://picsum.photos/400/320" alt="" srcset="" class=" rounded-2xl mt-56  " >
-            <iframe frameborder="0" height="475px" id="inlineTimatic" src="https://email.copa.com/p/2EUB-6KO/espromocionesweb" class="" title="TIMATIC" width="68%"></iframe>
-            
-          
+        <div class="flex flex-col lg:flex-row items-start">
+            <img src="https://picsum.photos/400/400" alt="" srcset="" class=" rounded-2xl mt-56  " >
+            <div class="flex-1">
+                <h3 class="!font-heading font-heading-bold text-primary text-h3 md:text-h3-lg pl-48  pt-64  ">Completa tus datos para recibir nuestras promociones
+                </h3>
+                <iframe frameborder="0" height="700" id="inlineTimatic" src="https://email.copa.com/p/2EUB-6KO/espromocionesweb" class="" title="TIMATIC" width="100%"></iframe>
+            </div>
         </div>
        
         
